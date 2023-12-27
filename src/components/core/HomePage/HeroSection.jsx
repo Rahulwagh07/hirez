5b10e38b4ee35f0cd9f91df2fce6e0cba3f65b51
Rahulwagh07 <121787635+Rahulwagh07@users.noreply.github.com>
2023-12-27 23:53:28 +0530

    Update HeroSection.jsx
    fixed typo error

--- src/components/core/HomePage/HeroSection.jsx
+++ src/components/core/HomePage/HeroSection.jsx
@@ -7,8 +7,8 @@ function HeroSection() {
     <div className='w-10/12 flex mt-20 gap-20'>
         <div className='flex flex-col gap-10 w-6/12'>
             <h2 className='text-5xl text-blue-150'>Find the best talent for your content creation</h2>
-            <p className='text-lg '>Connect With skilled video editors, scripts writeres, thumbnail designers, and more.
-            Post a job or search for a talentes professionals on Hire Studi</p>
+            <p className='text-lg '>Hire the skilled video editors, scripts writeres, thumbnail designers, and more.
+            Post a job or search for a talented professionals on HireUp</p>
             <div className='flex gap-8'>
                 <CTAButton active={true} linkto={"/signup"}>
                   Get Started
