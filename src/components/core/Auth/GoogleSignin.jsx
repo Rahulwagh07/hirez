c6c6c05cb1bb3395a5d71e4d4a11cfc132e2d1b8
Rahulwagh07 <rahul.wagh21@vit.edu>
2023-12-30 04:03:02 +0530

    google auth

--- src/components/core/Auth/GoogleSignin.jsx
+++ src/components/core/Auth/GoogleSignin.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
-import { FcGoogle } from "react-icons/fc";
+import React, { useEffect } from 'react';
+import { FcGoogle } from 'react-icons/fc';
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID
+/* global google */
 
 function GoogleSignin() {
+  const handleGoogleLogin = () => {
+  };
 
-    const handleGoogleLogin = () => {
-        //to do 
-    };
+  useEffect(() => {
+    google.accounts.id.initialize({
+      client_id: CLIENT_ID,
+    });
+  }, []);
 
   return (
-    <button onClick={handleGoogleLogin} className='bg-black text-richblack-25 py-2 px-4 rounded flex items-center justify-center mb-4 lg:w-[325px] h-[50px]'>
-        <FcGoogle className='mr-4' />
-        <p className='font-semibold'>Continue with Google</p>
+    <button
+      onClick={() => {
+        google.accounts.id.prompt(handleGoogleLogin);
+      }}
+      className='bg-black text-richblack-25 py-2 px-4 rounded flex items-center justify-center mb-4 lg:w-[325px] h-[50px]'
+    >
+      <FcGoogle className='mr-4' />
+      <p className='font-semibold'>Continue with Google</p>
     </button>
-  )
+  );
 }
 
-export default GoogleSignin
+export default GoogleSignin;
